3cf41dd9769f36e1d2e3afe0e1a90d9e58e56db9
GAierken <g.aierken@gmail.com>
2020-09-17 23:30:46 -0400

    added google fonts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Header } from 'semantic-ui-react'
 function App() {
   return (
     <div className="App">
-      <Header as="h1" style={{color: 'blue', "text-align": 'center'}}>Uyghur Kids' Books Project</Header>
+      <Header as="h1" style={{color: 'blue', "text-align": 'center', 'font-family': "'Indie Flower', cursive"}}>Uyghur Kids' Books Project</Header>
     </div>
   );
 }
