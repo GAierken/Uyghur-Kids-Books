694d1f932b57534c11326f38bc53f86a3f217283
GAierken <g.aierken@gmail.com>
2020-11-12 22:16:20 -0500

    added bookcontainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Header } from 'semantic-ui-react'
-
+import BookContainer from './Components/BookContainer'
 
 function App() {
   return (
     <div className="App">
       <Header as="h1" style={{color: '#00d5ff', "text-align": 'center', 'font-family': "'Indie Flower', cursive"}}>Uyghur Kids' Books Project</Header>
+      <BookContainer/>
     </div>
   );
 }
---
+++ src/Components/BookContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BookContainer() {
+    return (
+        <div>
+            Some books
+        </div>
+    )
+}
